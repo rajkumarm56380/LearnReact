157a4cf3e09c81e2454e5593d058bfb5e5f251a9
rajkumar.m56380 <rajkumar.m56380@gmail.com>
2026-08-19 22:11:14 +0530

    First commit

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
-import { SymbolView } from 'expo-symbols';
-import { Link, Tabs } from 'expo-router';
-import { Platform, Pressable } from 'react-native';
+import { Link, Tabs } from "expo-router";
+import { SymbolView } from "expo-symbols";
+import { Pressable } from "react-native";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { useClientOnlyValue } from "@/src/app/components/useClientOnlyValue";
+import { useColorScheme } from "@/src/app/components/useColorScheme";
+import Colors from "@/src/app/constants/Colors";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -16,17 +16,18 @@ export default function TabLayout() {
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: "Tab One",
           tabBarIcon: ({ color }) => (
             <SymbolView
               name={{
-                ios: 'chevron.left.forwardslash.chevron.right',
-                android: 'code',
-                web: 'code',
+                ios: "chevron.left.forwardslash.chevron.right",
+                android: "code",
+                web: "code",
               }}
               tintColor={color}
               size={28}
@@ -37,7 +38,7 @@ export default function TabLayout() {
               <Pressable style={{ marginRight: 15 }}>
                 {({ pressed }) => (
                   <SymbolView
-                    name={{ ios: 'info.circle', android: 'info', web: 'info' }}
+                    name={{ ios: "info.circle", android: "info", web: "info" }}
                     size={25}
                     tintColor={Colors[colorScheme].text}
                     style={{ opacity: pressed ? 0.5 : 1 }}
@@ -51,13 +52,13 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: "Tab Two",
           tabBarIcon: ({ color }) => (
             <SymbolView
               name={{
-                ios: 'chevron.left.forwardslash.chevron.right',
-                android: 'code',
-                web: 'code',
+                ios: "chevron.left.forwardslash.chevron.right",
+                android: "code",
+                web: "code",
               }}
               tintColor={color}
               size={28}

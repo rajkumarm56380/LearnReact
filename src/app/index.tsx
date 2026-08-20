55fef328e8b58f5b652a1a702e2e0ed7d35ed23d
rajkumar.m56380 <rajkumar.m56380@gmail.com>
2026-08-20 22:14:03 +0530

    Assignment - Auth

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,6 +11,9 @@ const index = () => {
       <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
+      <Link href={"/sign-in"} asChild>
+        <Button text="Sign in" />
+      </Link>
     </View>
   );
 };
